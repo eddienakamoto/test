34b63a7c4cd3dafc905832020b729411699b7877
Eddie Ciccone <eddie@nakamotomining.ai>
2024-09-27 13:57:47 -0500

    test

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+_summary_
+"""
+
+if __name__ == "__main__":
+    s: str = (
+        "This is just some random text, there will be giberish asdf asdf"
+        + " asdf asd fasdf asd fasd fasdf ads fas dfa sdf adsf"
+    )
+
+    s += "hola"
+
+    fail = "This will be longer and unaffected by black: adsf asdfas fdas fdas dfas fdas dfa sfd asdf asdf asdf sdf adfasff asdfasdf"
+    print(s)
